3ee921286e6e028c9b25a0e85705fe871d7b6415
shubham pathak <121822895+shubham13101996@users.noreply.github.com>
2023-05-03 02:22:27 +0530

    resume_build_ui

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { HStack, Link, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <HStack
+      p={3}
+      bg="gray.900"
+      justify="center"
+      position="fixed"
+      w="100%"
+      bottom={0}
+    >
+      <Text color="white">
+        Designed and developed by{" "}
+        <Link
+          href="https://github.com/shubham13101996"
+          isExternal
+          color="cyan.200"
+        >
+          shubham pathak <ExternalLinkIcon />
+        </Link>{" "}
+      </Text>
+    </HStack>
+  );
+};
+
+export default Footer;
